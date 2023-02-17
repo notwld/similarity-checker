af82f0ce7e080d4f47b45ee17e3472b7f1cd1d8d
notwld <mwfarrukh@gmail.com>
2023-02-17 22:36:17 +0500

    test

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import re
-import PyPDF2
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from os import system as s
+try:
+    from PyPDF2 import PdfReader
+    import nltk
 
+    from nltk.corpus import stopwords
+    from nltk.tokenize import word_tokenize
+
+except ImportError as e:
+    print("Modules are not installed!\nInstalling modules...")
+    s("pip install nltk")
+    s("pip install PyPDF2")
+
+s("cls")
 
 def remove_redundant_words(text):
     words = word_tokenize(text)
@@ -14,24 +23,24 @@ def remove_redundant_words(text):
 
 
 def extract_pdf_text(pdf_file):
+    
     with open(pdf_file, 'rb') as file:
-        pdf = PyPDF2.PdfFileReader(file)
+        pdf = PdfReader(file)
+        number_of_pages = len(pdf.pages)
         text = ''
-        for page in range(pdf.getNumPages()):
-            text += pdf.getPage(page).extractText()
+        for page in range(number_of_pages):
+            text += pdf.pages[page].extract_text()
+
     return text
 
 
 def jaccard_similarity(text1, text2):
-    text1 = remove_redundant_words(re.sub(r'[^\w\s]', '', text1.lower()))
-    text2 = remove_redundant_words(re.sub(r'[^\w\s]', '', text2.lower()))
-
-    text1 = set(text1.split())
-    text2 = set(text2.split())
-
+    text1,text2  = set(remove_redundant_words(re.sub(r'[^\w\s]', '', text1.lower())).split()),set(remove_redundant_words(re.sub(r'[^\w\s]', '', text2.lower())).split())
     intersection = text1.intersection(text2)
     union = text1.union(text2)
     jaccard_index = len(intersection) / len(union)
 
-    return jaccard_index
+    return round(jaccard_index, 2)*100
 
+pdf1,pdf2 = extract_pdf_text("1.pdf"),extract_pdf_text("2.pdf")
+print(jaccard_similarity(pdf1,pdf2))
